4c98cf77f71a94aabb08db1c4b8ddb3c52de695a
Airi Shiina <c17.dev@gmail.com>
2018-12-11 22:22:50 +0900

    Modify methods code style
    - convert tab to space
    - chenge the branch line
    - add returning the 500 when there is an error

--- main.go
+++ main.go
@@ -1,56 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+    "io/ioutil"
+    "log"
+    "net/http"
 
-	simplejson "github.com/bitly/go-simplejson"
-	"github.com/gin-gonic/gin"
+    simplejson "github.com/bitly/go-simplejson"
+    "github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
 
 func main() {
-	router = gin.Default()
-	initializeRoutes()
-	router.Run(":8088")
+    router = gin.Default()
+    initializeRoutes()
+    router.Run(":8088")
 }
 
 func initializeRoutes() {
-	router.GET("/foo", getFoo)
-	router.GET("/bar", getBar)
-	router.GET("/car/:id", getCar)
+    router.GET("/foo", getFoo)
+    router.GET("/bar", getBar)
+    router.GET("/car/:id", getCar)
 }
 
 func getFoo(c *gin.Context) {
-	bytes, e := ioutil.ReadFile(`./static/json/get-foo.json`)
-	if e != nil {
-		log.Fatal(e)
-	}
-	data, e := simplejson.NewJson(bytes)
-	fmt.Println(data)
-	c.JSON(http.StatusOK, data)
+    if bytes, e := ioutil.ReadFile(`./static/json/get-foo.json`); e == nil {
+        data, _ := simplejson.NewJson(bytes)
+        c.JSON(http.StatusOK, data)
+    } else {
+        log.Fatal(e)
+        c.JSON(http.StatusInternalServerError, gin.H{"message": "Server Error"})
+    }
 }
 
 func getBar(c *gin.Context) {
-	bytes, e := ioutil.ReadFile(`./static/json/get-bar.json`)
-	if e != nil {
-		log.Fatal(e)
-	}
-	data, e := simplejson.NewJson(bytes)
-	fmt.Println(data)
-	c.JSON(http.StatusOK, data)
+    if bytes, e := ioutil.ReadFile(`./static/json/get-bar.json`); e == nil {
+        data, _ := simplejson.NewJson(bytes)
+        c.JSON(http.StatusOK, data)
+    } else {
+        log.Fatal(e)
+        c.JSON(http.StatusInternalServerError, gin.H{"message": "Server Error"})
+    }
 }
 
 func getCar(c *gin.Context) {
-	id := c.Param("id")
-	bytes, e := ioutil.ReadFile(`./static/json/car/get-car` + id + `.json`)
-	if e != nil {
-		log.Fatal(e)
-	}
-	data, e := simplejson.NewJson(bytes)
-	fmt.Println(data)
-	c.JSON(http.StatusOK, data)
+    id := c.Param("id")
+    if bytes, e := ioutil.ReadFile(`./static/json/car/get-car` + id + `.json`); e == nil {
+        data, _ := simplejson.NewJson(bytes)
+        c.JSON(http.StatusOK, data)
+    } else {
+        log.Fatal(e)
+        c.JSON(http.StatusInternalServerError, gin.H{"message": "Server Error"})
+    }
 }
